fe4549fcd7f38246fc0b3995a3755a64d329d972
Fleming Patel <flpa5726@colorado.edu>
2024-08-12 23:48:52 -0400

    assignment-5-complete local tests are passed

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,13 +6,14 @@
 ##############################################################
 
 
-AESD_ASSIGNMENTS_VERSION = 6802f49
+AESD_ASSIGNMENTS_VERSION = c16de0c
 AESD_ASSIGNMENTS_SITE = "git@github.com:cu-ecen-aeld/assignments-3-and-later-flemingpatel.git"
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) CC="$(TARGET_CC)" -C $(@D)/server all
 endef
 
 
@@ -23,6 +24,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/finder.sh
 	$(INSTALL) -D -m 755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/finder-test.sh
 	$(INSTALL) -D -m 755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/writer
+	$(INSTALL) -D -m 755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+	$(INSTALL) -D -m 755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 
 	# modifying scripts
 	sed -i 's|conf/username.txt|/etc/finder-app/conf/username.txt|g' $(TARGET_DIR)/usr/bin/finder-test.sh
@@ -31,6 +34,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	sed -i 's|\./finder.sh|/usr/bin/finder.sh|g' $(TARGET_DIR)/usr/bin/finder-test.sh
 	sed -i 's|bash|sh|g' $(TARGET_DIR)/usr/bin/finder.sh
 	sed -i '/OUTPUTSTRING=$(/usr/bin/finder.sh "$$WRITEDIR" "$$WRITESTR")/a echo "\$${OUTPUTSTRING}" > /tmp/assignment4-result.txt' $(TARGET_DIR)/usr/bin/finder-test.sh
+	sed -i 's|bash|sh|g' $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
+
